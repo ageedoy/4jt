49e5122a05711095671c624d35e778da6cc2f8e6
Kenneth Yan <kyan@ebay.com>
2015-10-12 15:44:18 -0700

    adding chai

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var assert = require("assert");
+var chai = require("chai");
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,4 +8,19 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});
+
+describe('Hello', function() {
+  describe('substring', function () {
+    it('check for substrings and arrays', function () {
+	var assert = chai.assert;
+	var foo ="bar";
+	var tea = {'flavors':["choco","vanilla","strawberry"]};
+	assert.typeOf(foo, 'string');
+	assert.equal(foo, 'bar');
+	assert.lengthOf(foo, 3)
+	assert.property(tea, 'flavors');
+	assert.lengthOf(tea.flavors, 3);
+		});
+	});
 });
